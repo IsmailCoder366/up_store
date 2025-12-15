01a2acc0e9f486e166febf1736894c5585e39747
Muhammad Ismail <ismailcoder366@gmail.com>
2025-12-15 23:55:11 +0500

    user profile setting menu customized

--- lib/personalization/screens/widgets/settings_menu_tile.dart
+++ lib/personalization/screens/widgets/settings_menu_tile.dart
@@ -3,15 +3,19 @@ import 'package:iconsax/iconsax.dart';
 
 class SettingMenuTile extends StatelessWidget {
   const SettingMenuTile({
-    super.key,
+    super.key, required this.title, required this.subTitle, required this.icon,
   });
+
+  final String title, subTitle;
+  final IconData icon;
+
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Icon(Iconsax.safe_home),
-      title: Text('My Addressed', style: Theme.of(context).textTheme.titleMedium),
-      subtitle: Text('Set shopping delivery addresses', style: Theme.of(context).textTheme.labelMedium),
+      leading: Icon(icon),
+      title: Text(title, style: Theme.of(context).textTheme.titleMedium),
+      subtitle: Text(subTitle, style: Theme.of(context).textTheme.labelMedium),
     );
   }
 }
