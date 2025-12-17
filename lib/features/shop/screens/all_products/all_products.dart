b1425034b32c620c9320a72f99f84f43c4e612fb
Muhammad Ismail <ismailcoder366@gmail.com>
2025-12-17 20:10:08 +0500

    firebase setup completed, packages added

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:up_store/common/styles/padding.dart';
 import 'package:up_store/common/widgets/app_bar/app_bar.dart';
-import 'package:up_store/common/widgets/layout/grid_layout.dart';
-import 'package:up_store/common/widgets/products/product_card/product_card_vertical.dart';
+
 import 'package:up_store/common/widgets/products/sortable_products.dart';
-import 'package:up_store/utils/constants/sizes.dart';
+
 
 class AllProductsScreen extends StatelessWidget {
   const AllProductsScreen({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:up_store/firebase_options.dart';
 
 import 'my_app.dart' show MyApp;
 
-void main() {
+void main() async{
 
     /// widgets flutter binding
-  // final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   /// flutter native splash
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
